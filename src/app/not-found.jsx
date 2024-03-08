270a646c1fa7fb218880cb60f5ac4fccf8de4539
Chirag Bang <chiragbang33@gmail.com>
2024-03-08 15:32:07 +0530

    Initial commit: Added Project files

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <>
+    <h2>Not Found</h2>
+    <p>Sorry, the Page you are looking for does not exist.</p>
+    <Link href="/">Return Home</Link>
+    </>
+  )
+}
+
+export default NotFound
